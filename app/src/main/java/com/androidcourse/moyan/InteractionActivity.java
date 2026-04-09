07611938286361c016616b154b248df8074de6bd
Ziqian Chen <3100313027@qq.com>
2026-04-09 11:40:15 +0800

    Revert "[C012]界面优化"
    This reverts commit 808c63af98d76a06f1a6dc66eb9ce2630524c79f.

--- app/src/main/java/com/androidcourse/moyan/InteractionActivity.java
+++ app/src/main/java/com/androidcourse/moyan/InteractionActivity.java
@@ -1,8 +1,6 @@
 package com.androidcourse.moyan;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,59 +10,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class InteractionActivity extends AppCompatActivity {
 
-    private LinearLayout navHome, navExplore, navMessages, navProfile;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_interaction);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        initViews();
-        setupBottomNavigation();
-    }
-
-    private void initViews() {
-        navHome = findViewById(R.id.nav_home);
-        navExplore = findViewById(R.id.nav_explore);
-        navMessages = findViewById(R.id.nav_messages);
-        navProfile = findViewById(R.id.nav_profile);
-    }
-
-    private void setupBottomNavigation() {
-        if (navHome != null) {
-            navHome.setOnClickListener(v -> {
-                Intent intent = new Intent(InteractionActivity.this, HomeActivity.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            });
-        }
-
-        if (navExplore != null) {
-            navExplore.setOnClickListener(v -> {
-                // 当前页面，不做跳转
-            });
-        }
-
-        if (navMessages != null) {
-            navMessages.setOnClickListener(v -> {
-                Intent intent = new Intent(InteractionActivity.this, MessageActivity.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            });
-        }
-
-        if (navProfile != null) {
-            navProfile.setOnClickListener(v -> {
-                Intent intent = new Intent(InteractionActivity.this, ProfileActivity.class);
-                startActivity(intent);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            });
-        }
     }
 }
